ed8a70ca9a88f389d415d0895f7387462fc453c9
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-25 16:23:19 +0600

    push

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
     origin: [
       'http://192.168.10.16:3000',
       "http://localhost:3000",
-      "http://10.0.60.52:3000",
+      "http://10.0.60.52:3000",'http://13.62.48.83:5173',
       "http://13.62.48.83:3000",
       "http://10.0.60.199:3000",
       "https://al-ansari-website-integration.vercel.app"
